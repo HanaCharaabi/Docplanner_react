1f05779b6d7422dd2fc97878407329bc125fbf06
hana Charaabi <charaabihana90@gmail.com>
2019-05-09 14:10:09 +0100

    Updates

---
+++ precache-manifest.324697b59a78eb7a1d7f9c53eeeb2cd6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b380b682f4bf97a078e03b2fff284c1",
+    "url": "/REACT-DOCPLANNER-CLONE.git/index.html"
+  },
+  {
+    "revision": "7be2798894d8bfe37b5a",
+    "url": "/REACT-DOCPLANNER-CLONE.git/static/css/main.99f51b6d.chunk.css"
+  },
+  {
+    "revision": "0974015e94b5eff7d38d",
+    "url": "/REACT-DOCPLANNER-CLONE.git/static/js/2.809ee080.chunk.js"
+  },
+  {
+    "revision": "7be2798894d8bfe37b5a",
+    "url": "/REACT-DOCPLANNER-CLONE.git/static/js/main.b83fa3b8.chunk.js"
+  },
+  {
+    "revision": "fa2387fc6f8e1369d3e7",
+    "url": "/REACT-DOCPLANNER-CLONE.git/static/js/runtime~main.f8251149.js"
+  },
+  {
+    "revision": "30fef32f18881c2811fe20f9d731bab2",
+    "url": "/REACT-DOCPLANNER-CLONE.git/static/media/Barcelona.30fef32f.png"
+  },
+  {
+    "revision": "2ab726ec8c70ef89de611916f1bd5a57",
+    "url": "/REACT-DOCPLANNER-CLONE.git/static/media/Curitiba.2ab726ec.png"
+  },
+  {
+    "revision": "a772a418e427f7945447d5e0f8f185cf",
+    "url": "/REACT-DOCPLANNER-CLONE.git/static/media/Istanbul.a772a418.png"
+  },
+  {
+    "revision": "c92e8f5856e4439678226216d400b01c",
+    "url": "/REACT-DOCPLANNER-CLONE.git/static/media/Mexico City.c92e8f58.png"
+  },
+  {
+    "revision": "16430b36ccdd96c0b1fd2757733f29bb",
+    "url": "/REACT-DOCPLANNER-CLONE.git/static/media/Rome.16430b36.png"
+  },
+  {
+    "revision": "4a92ae162eaf6d061ee4f16ced26fc66",
+    "url": "/REACT-DOCPLANNER-CLONE.git/static/media/Warsaw.4a92ae16.png"
+  },
+  {
+    "revision": "edb44798eed9e9d14295955d5e44963a",
+    "url": "/REACT-DOCPLANNER-CLONE.git/static/media/background.edb44798.jpg"
+  },
+  {
+    "revision": "8e5b345ff9df62455087a329d3b48498",
+    "url": "/REACT-DOCPLANNER-CLONE.git/static/media/logo-default-group-en.8e5b345f.svg"
+  },
+  {
+    "revision": "82d323de468a8ad9636c43e8de2a26ba",
+    "url": "/REACT-DOCPLANNER-CLONE.git/static/media/screen-marketplace.82d323de.png"
+  },
+  {
+    "revision": "120076c7e404fb282e53f6885f38dac7",
+    "url": "/REACT-DOCPLANNER-CLONE.git/static/media/screen-saas.120076c7.png"
+  }
+]);
